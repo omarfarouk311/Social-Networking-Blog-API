6cf01c5751051b03ffd9e7a7684333e6574d323b
Omar <omarfarouk219@gmail.com>
2024-07-01 03:58:17 +0300

    Create post class and add create and fetchPosts methods

---
+++ models/post.js
@@ -0,0 +1,24 @@
+const { ObjectId } = require('mongodb');
+const { getDb } = require('../util/database.js');
+
+module.exports = class Post {
+    constructor({ _id, title, content, imageUrl, creator, createdAt }) {
+        this._id = _id;
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.creator = creator;
+        this.createdAt = createdAt;
+    }
+
+    async save() {
+        const db = getDb();
+        const { insertedId } = await db.collection('posts').insertOne(this);
+        this._id = insertedId;
+    }
+
+    static async fetchPosts() {
+        const db = getDb();
+        return await db.collection('posts').find().toArray();
+    }
+}
