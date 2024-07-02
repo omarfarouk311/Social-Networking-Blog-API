4864655b1c34334e1896531cf0902b6d805bcedc
Omar <omarfarouk219@gmail.com>
2024-07-02 03:31:29 +0300

    fix a bug in error handling middleware

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -10,8 +10,8 @@ exports.notFound = (req, res, next) => {
     return next(err);
 };
 
-exports.errorHandlingMiddleware = (req, res, next, err) => {
+exports.errorHandlingMiddleware = (err, req, res, next) => {
     console.error(err);
     const statusCode = err.statusCode || 500;
-    res.status(statusCode).json({ message: err.message });
+    return res.status(statusCode).json({ message: err.message });
 };
