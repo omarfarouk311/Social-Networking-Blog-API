9876748f95288e4fd3f314669853d3581e4320bc
Omar <omarfarouk219@gmail.com>
2024-07-15 01:45:13 +0300

    Update post model

--- models/post.js
+++ models/post.js
@@ -3,13 +3,15 @@ const { getDb } = require('../util/database.js');
 const { promises: fsPromises } = require('fs');
 
 module.exports = class Post {
-    constructor({ _id, title, content, imageUrl, creator, createdAt }) {
+    constructor({ _id, title, content, imageUrl, creator, creationDate, tags, comments }) {
         this._id = _id;
         this.title = title;
         this.content = content;
         this.imageUrl = imageUrl;
         this.creator = creator;
-        this.createdAt = createdAt;
+        this.creationDate = creationDate;
+        this.tags = tags;
+        this.comments = comments;
     }
 
     async create() {
@@ -18,25 +20,33 @@ module.exports = class Post {
         this._id = insertedId;
     }
 
-    static fetchPosts(page, itemsPerPage) {
+    static async getPosts(page, itemsPerPage) {
         const db = getDb();
-        return db.collection('posts').find()
+        const results = await db.collection('posts').find()
             .skip((page - 1) * itemsPerPage)
             .limit(itemsPerPage)
+            .project({ content: 0, imageUrl: 0, comments: 0 })
             .toArray();
+
+        const promises = results.map(result => {
+            result.creator = db.collection('users').findOne({ _id: result.creator }).project({ name: 1, imageUrl: 1 });
+            return result.creator;
+        });
+        await Promise.all(promises);
+        return results;
     }
 
-    static fetchPost(postId) {
+    static getPost(postId) {
         return db.collection('posts').findOne({ _id: ObjectId.createFromHexString(postId) });
     }
 
     static countPosts() {
-        return db.collection('posts').find().countDocuments();
+        return db.collection('posts').countDocuments({});
     }
 
-    async delete(post) {
-        const { imageUrl } = post;
-        await db.collection('posts').deleteOne({ _id: post._id });
-        await fsPromises.unlink(imageUrl);
+    async delete() {
+        const { imageUrl } = this;
+        await db.collection('posts').deleteOne({ _id: this._id });
+        fsPromises.unlink(imageUrl).catch(err => console.error(err));
     }
 }
