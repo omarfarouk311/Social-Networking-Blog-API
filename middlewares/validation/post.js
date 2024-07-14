b27ff132bc67462d2046b576e6662261d42e7c43
Omar <omarfarouk219@gmail.com>
2024-07-14 21:09:47 +0300

    Move post existence check into validation folder

--- middlewares/validation/post.js
+++ middlewares/validation/post.js
@@ -1,11 +1,11 @@
-const Post = require('../models/post');
+const Post = require('../../models/post');
 
 exports.checkPostExistence = async (req, res, next) => {
-    const { postId } = (req.method === 'GET' ? req.params : req.body);
+    const { postId } = (req.method === 'GET' || req.method === 'DELETE' ? req.params : req.body);
     try {
         let post;
         if (postId.length === 24) {
-            post = await Post.fetchPost(postId);
+            post = await Post.getPost(postId);
         }
 
         if (!post) {
