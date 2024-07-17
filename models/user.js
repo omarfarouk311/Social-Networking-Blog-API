6a2726d80d6289f028c88729dc7ac79752fd146f
Omar <omarfarouk219@gmail.com>
2024-07-17 21:38:12 +0300

    update user model and create getUser and getUserPosts methods

--- models/user.js
+++ models/user.js
@@ -1,15 +1,22 @@
 const { getDb } = require('../util/database');
+const Post = require('./post');
 
 module.exports = class User {
-    constructor({ _id, email, password, name, imageUrl, following, followers, bookmarks }) {
+    constructor({ _id, email, password, name, imageUrl, followingIds, followersIds, bookmarksIds, postsIds,
+        commentsIds, likedPostsIds, bio, location }) {
         this._id = _id;
         this.email = email;
         this.password = password;
         this.name = name;
+        this.bio = bio;
+        this.location = location;
         this.imageUrl = imageUrl;
-        this.following = following;
-        this.followers = followers;
-        this.bookmarks = bookmarks
+        this.followingIds = followingIds;
+        this.followersIds = followersIds;
+        this.bookmarksIds = bookmarksIds;
+        this.postsIds = postsIds;
+        this.commentsIds = commentsIds;
+        this.likedPostsIds = likedPostsIds;
     }
 
     async create() {
@@ -18,5 +25,47 @@ module.exports = class User {
         this._id = insertedId;
     }
 
+    static async getUser(userId) {
+        const user = await db.collection('users').aggregate([
+            {
+                $match: {
+                    _id: userId
+                }
+            },
+            {
+                $addFields: {
+                    followersCount: { $size: '$followersIds' },
+                    followingCount: { $size: '$followingIds' }
+                }
+            },
+            {
+                $project: {
+                    password: 0,
+                    bookmarksIds: 0,
+                    commentsIds: 0,
+                    email: 0,
+                    postsIds: { $slice: [0, 10] },
+                    followersIds: { $slice: [0, 20] },
+                    followingIds: { $slice: [0, 20] },
+                    likedPostsIds: { $slice: [0, 10] }
+                }
+            }
+        ]);
+
+        const filter = { _id: { $in: user.postsIds } };
+        user.posts = await this.getUserPosts(filter)
+        user.lastPostId = user.postsIds[user.postsIds.length - 1];
+        delete user.postsIds;
+
+        return user;
+    }
+
+    getUserPosts(filter) {
+        const db = getDb();
+        return db.collection('posts').find(filter)
+            .limit(10)
+            .project({ content: 0, imagesUrls: 0, commentsIds: 0, creatorId: 0 })
+            .toArray();
+    }
 
 }
