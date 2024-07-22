0f76456b2b4334884040b20a61043dc17178b8d1
Omar <omarfarouk219@gmail.com>
2024-07-22 23:39:16 +0300

    create two endpoints for retrieving and creating comments on a post

--- routes/feed.js
+++ routes/feed.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { notAllowed } = require('../middlewares/errors');
 const feedController = require('../controllers/feed');
+const commentsController = require('../controllers/comments');
 const { checkPostExistence } = require('../middlewares/validation/post');
 const upload = require('../util/multer configurations');
 const router = Router();
@@ -8,12 +9,17 @@ const router = Router();
 router.route('/')
     .get(feedController.getPosts)
     .post(upload.array('images', 10), feedController.createPost)
-    .all(notAllowed)
+    .all(notAllowed);
 
 router.route('/:postId')
     .get(checkPostExistence, feedController.getPost)
     .delete(checkPostExistence, feedController.deletePost)
     .patch(checkPostExistence, feedController.updatePost)
+    .all(notAllowed);
+
+router.route('/:postId/comments')
+    .get(commentsController.getComments)
+    .post(commentsController.createComment)
     .all(notAllowed);
 
 module.exports = router;
