451cd34da5828ce396dd25e0c1c1ae4ecc0cd412
Omar <omarfarouk219@gmail.com>
2024-07-23 02:29:06 +0300

    validate post creation data

--- middlewares/validation/post.js
+++ middlewares/validation/post.js
@@ -1,12 +1,13 @@
 const Post = require('../../models/post');
 const { ObjectId } = require('mongodb');
+const { checkExact, body, validationResult } = require('express-validator');
 
 exports.checkPostExistence = async (req, res, next) => {
-    const { postId } = (req.method === 'GET' || req.method === 'DELETE' ? req.params : req.body);
+    const { postId } = req.params;
     try {
         let post;
         if (postId.length === 24) {
-            post = await Post.getPost(ObjectId.createFromHexString(postId));
+            post = await Post.getPost({ _id: ObjectId.createFromHexString(postId) });
         }
 
         if (!post) {
@@ -22,3 +23,25 @@ exports.checkPostExistence = async (req, res, next) => {
         return next(err);
     }
 };
+
+exports.validatePostCreation = [
+    body('title')
+        .trim()
+        .isString()
+        .withMessage("Post title must be a string")
+        .notEmpty()
+        .withMessage("Post title can't be empty")
+        .isLength({ min: 5, max: 20 })
+        .withMessage("Title length must be between 5 to 20 characters")
+    ,
+    body('content')
+        .trim()
+        .isString()
+        .withMessage("Post content must be a string")
+        .notEmpty()
+        .withMessage("Post content can't be empty")
+        .isLength({ max: 1000 })
+        .withMessage("Post content can't exceed 1000 character")
+    ,
+    checkExact([], { message: 'Request structure is invalid because too many fields are passed', locations: ['body', 'query'] })
+];
