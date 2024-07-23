81e8f6e2f4822baaef5fe46754ee4e5af2085313
Omar <omarfarouk219@gmail.com>
2024-07-23 04:41:08 +0300

    validate post updating and update validation errors handling

--- middlewares/validation/post.js
+++ middlewares/validation/post.js
@@ -31,8 +31,8 @@ exports.validatePostCreation = [
         .withMessage("Post title must be a string")
         .notEmpty()
         .withMessage("Post title can't be empty")
-        .isLength({ min: 5, max: 20 })
-        .withMessage("Title length must be between 5 to 20 characters")
+        .isLength({ min: 5, max: 30 })
+        .withMessage("Title length must be between 5 to 30 characters")
     ,
     body('content')
         .trim()
@@ -46,9 +46,60 @@ exports.validatePostCreation = [
     checkExact([], { message: 'Request structure is invalid because too many fields are passed', locations: ['body', 'query'] })
 ];
 
+exports.validatePostUpdating = [
+    body('modifyLikes')
+        .optional()
+        .isBoolean()
+        .withMessage("modifyLikes value must be a boolean value")
+    ,
+    body('value')
+        .optional()
+        .isInt({ allow_leading_zeroes: false, max: 1, min: -1 })
+        .withMessage('value must be 1 or -1')
+    ,
+    body('title')
+        .optional()
+        .trim()
+        .isString()
+        .withMessage("Post title must be a string")
+        .notEmpty()
+        .withMessage("Post title can't be empty")
+        .isLength({ min: 5, max: 30 })
+        .withMessage("Title length must be between 5 to 30 characters")
+    ,
+    body('content')
+        .optional()
+        .trim()
+        .isString()
+        .withMessage("Post content must be a string")
+        .notEmpty()
+        .withMessage("Post content can't be empty")
+        .isLength({ max: 1000 })
+        .withMessage("Post content can't exceed 1000 character")
+    ,
+    body('tags')
+        .optional()
+        .isArray()
+        .withMessage("Post tags must be an array")
+    ,
+    body('tags.*')
+        .optional()
+        .isString()
+        .withMessage("Tags value must be a string")
+    ,
+    checkExact([], { message: 'Request structure is invalid because too many fields are passed', locations: ['body', 'query'] })
+];
+
 exports.handleValidationErrors = (req, res, next) => {
-    const errors = validationResult(req);
+    let errors = validationResult(req);
+
     if (!errors.isEmpty()) {
+        errors = errors.array();
+        const err = errors.find(err => err.type === 'unknown_fields');
+        if (err) {
+            err.statusCode = 400;
+            return next(err);
+        }
         return next(errors.array());
     }
     next();
