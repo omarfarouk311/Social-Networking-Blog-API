3d95260e3236f3d39e02f6ec9860caf207df2779
Omar <omarfarouk219@gmail.com>
2024-07-26 22:39:49 +0300

    update comments endpoints by adding validation middlewares

--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const { notAllowed } = require('../middlewares/errors');
 const feedController = require('../controllers/feed');
+const userController = require('../controllers/user');
 const commentsController = require('../controllers/comments');
 const { checkPostExistence, validateLikesUpdating, validatePostCreation, validatePostUpdating,
     handleValidationErrors } = require('../middlewares/validation/post');
+const { checkCommentExistence, validateCommentCreation, validateCommentUpdating } = require('../middlewares/validation/comments');
 const upload = require('../util/multer configurations');
 const router = Router();
 
@@ -15,18 +17,19 @@ router.route('/')
 router.route('/:postId')
     .get(checkPostExistence, feedController.getPost)
     .put(checkPostExistence, validatePostUpdating, upload.array('images', 15), handleValidationErrors, feedController.updatePost)
-    .patch(checkPostExistence, validateLikesUpdating, handleValidationErrors, feedController.updateLikes)
+    .patch(checkPostExistence, validateLikesUpdating, handleValidationErrors, userController.updatePostLikes)
     .delete(checkPostExistence, feedController.deletePost)
     .all(notAllowed);
 
 router.route('/:postId/comments')
     .get(commentsController.getComments)
-    .post(commentsController.createComment)
+    .post(validateCommentCreation, handleValidationErrors, commentsController.createComment)
     .all(notAllowed);
 
 router.route('/:postId/:commentId')
-    .patch(commentsController.updateComment)
-    .delete(commentsController.deleteComment)
+    .put(checkCommentExistence, validateCommentUpdating, handleValidationErrors, commentsController.updateComment)
+    .patch(checkCommentExistence, validateLikesUpdating, handleValidationErrors, userController.updateCommentLikes)
+    .delete(checkCommentExistence, commentsController.deleteComment)
     .all(notAllowed);
 
 module.exports = router;
