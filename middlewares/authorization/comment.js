bb1285787c5dafe4ef6963a73689c71a507202ee
Omar <omarfarouk219@gmail.com>
2024-08-04 19:07:43 +0300

    create comment modification authorization module

---
+++ middlewares/authorization/comment.js
@@ -0,0 +1,34 @@
+const Comment = require('../../models/comment');
+
+function authorizeCommentModification(operation) {
+    return async (req, res, next) => {
+        const { commentId, postId } = req.params, { userId } = req;
+        const projection = operation ? { _id: 1 } : { likingUsersIds: 1, parentId: 1, postId: 1 };
+
+        try {
+            const comment = await Comment.getComment({ creatorId: userId, postId }).project(projection);
+
+            if (!comment) {
+                const exist = await Comment.getComment({ postId, _id: commentId }).project({ _id: 1 });
+                if (exist) {
+                    return res.status(403).json({
+                        message: (operation ? 'Unauthorized request to update the comment' : 'Unauthorized request to delete the comment')
+                    });
+                }
+                return res.status(404).json({
+                    message: 'Comment not found'
+                });
+            }
+
+            req.comment = new Comment(comment);
+            return next();
+        }
+        catch (err) {
+            return next(err);
+        }
+    };
+}
+
+exports.authorizeCommentDeletion = authorizeCommentModification(0);
+
+exports.authorizeCommentUpdating = authorizeCommentModification(1);
